6f09a5a6fdba46c21193cd6ae2fedfe666b3f858
dannd1 <dannd1@viettel.com.vn>
2025-08-18 16:38:49 +0700

    .

--- component-template/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ component-template/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,12 +1,21 @@
-import { Component } from "@angular/core";
+import { Component, ElementRef, ViewChild } from "@angular/core";
 import { ButtonComponent } from "../../../shared/button/button.component";
 import { ControlComponent } from "../../../shared/control/control.component";
+import { FormsModule } from "@angular/forms";
 
 @Component({
   selector: "app-new-ticket",
   standalone: true,
-  imports: [ButtonComponent, ControlComponent],
+  imports: [ButtonComponent, ControlComponent, FormsModule],
   templateUrl: "./new-ticket.component.html",
   styleUrl: "./new-ticket.component.css",
 })
-export class NewTicketComponent {}
+export class NewTicketComponent {
+  @ViewChild("form") form?: ElementRef<HTMLFormElement>;
+
+  onSubmit(title: string, request: string) {
+    console.log("Title:", title);
+    console.log("Request:", request);
+    this.form?.nativeElement.reset();
+  }
+}
