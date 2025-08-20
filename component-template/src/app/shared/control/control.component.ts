131103193a6ddbcfcd438c87b952ae158ad70af9
dannd1 <dannd1@viettel.com.vn>
2025-08-20 17:40:36 +0700

    contentChild

--- component-template/src/app/shared/control/control.component.ts
+++ component-template/src/app/shared/control/control.component.ts
@@ -1,4 +1,12 @@
-import { Component, input, ViewEncapsulation } from "@angular/core";
+import {
+  Component,
+  contentChild,
+  ContentChild,
+  ElementRef,
+  inject,
+  input,
+  ViewEncapsulation,
+} from "@angular/core";
 
 @Component({
   selector: "app-control",
@@ -7,7 +15,24 @@ import { Component, input, ViewEncapsulation } from "@angular/core";
   templateUrl: "./control.component.html",
   styleUrl: "./control.component.css",
   encapsulation: ViewEncapsulation.None,
+  host: {
+    click: "onClick()",
+  },
 })
 export class ControlComponent {
   title = input.required<string>();
+  private el = inject(ElementRef);
+  @ContentChild("content") private control?: ElementRef<
+    HTMLInputElement | HTMLTextAreaElement
+  >;
+
+  private control2 =
+    contentChild<ElementRef<HTMLInputElement | HTMLTextAreaElement>>("content");
+
+  onClick() {
+    console.log("Control clicked");
+    console.log("Host element:", this.el);
+    console.log("Control value:", this.control?.nativeElement.value);
+    console.log("Control2 value:", this.control2()?.nativeElement.value);
+  }
 }
