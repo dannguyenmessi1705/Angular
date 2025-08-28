e223c63b39a04d42833ecf038ce76c6a9497d64e
dannd1 <dannd1@viettel.com.vn>
2025-08-28 16:34:49 +0700

    .

--- form/src/app/auth/login/login.component.ts
+++ form/src/app/auth/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, DestroyRef, inject, OnInit } from "@angular/core";
 import {
   AbstractControl,
   FormControl,
@@ -6,6 +6,7 @@ import {
   ReactiveFormsModule,
   Validators,
 } from "@angular/forms";
+import { debounceTime, delay, map, of } from "rxjs";
 
 function mustContainQuestionMark(control: AbstractControl) {
   if (control.value.includes("?")) {
@@ -14,6 +15,27 @@ function mustContainQuestionMark(control: AbstractControl) {
   return { notContainQuestionMark: true }; // Invalid
 }
 
+function isNotDuplicate(control: AbstractControl) {
+  const email = control.value;
+  return of(email).pipe(
+    delay(1000), // Giả lập đang gọi đến backend check trùng (trễ 1s)
+    map((email) => {
+      if (email === "messiprohy@gmail.com") return { isDuplicate: true };
+      return null;
+    })
+  );
+}
+
+/**
+ * Dùng init set vào FormControl
+ * */
+let initialEmailValue = "";
+const savedForm = window.localStorage.getItem("saved-login-form");
+
+if (savedForm) {
+  const loadedForm = JSON.parse(savedForm);
+  initialEmailValue = loadedForm.email;
+}
 @Component({
   selector: "app-login",
   standalone: true,
@@ -21,13 +43,20 @@ function mustContainQuestionMark(control: AbstractControl) {
   styleUrl: "./login.component.css",
   imports: [ReactiveFormsModule],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  destroyRef = inject(DestroyRef);
+
   form = new FormGroup({
-    email: new FormControl("", {
+    email: new FormControl(initialEmailValue, {
       validators: [Validators.required, Validators.email],
+      asyncValidators: [isNotDuplicate],
     }),
     password: new FormControl("", {
-      validators: [Validators.required, Validators.minLength(6)],
+      validators: [
+        Validators.required,
+        Validators.minLength(6),
+        mustContainQuestionMark,
+      ],
     }),
   });
 
@@ -45,6 +74,29 @@ export class LoginComponent {
       this.form.controls.password.dirty &&
       this.form.controls.password.invalid
     );
+  }
+
+  ngOnInit(): void {
+    /** Dùng patchValue */
+    // const savedForm = window.localStorage.getItem("saved-login-form");
+
+    // if (savedForm) {
+    //   const loadedForm = JSON.parse(savedForm);
+    //   this.form.patchValue({
+    //     email: loadedForm.email,
+    //   });
+    // }
+    const sub = this.form.valueChanges.pipe(debounceTime(500)).subscribe({
+      next: (value) => {
+        window.localStorage.setItem(
+          "saved-login-form",
+          JSON.stringify({ email: value.email })
+        );
+      },
+    });
+    this.destroyRef.onDestroy(() => {
+      sub.unsubscribe();
+    });
   }
 
   onSubmit() {
