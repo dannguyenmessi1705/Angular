bbfbfadcd2c88cf10c802753676ca21ce16c22b5
dannguyenmessi1705 <didannguyen17@gmail.com>
2025-08-30 02:31:21 +0700

    .

--- routing/src/app/users/user-tasks/user-tasks.component.ts
+++ routing/src/app/users/user-tasks/user-tasks.component.ts
@@ -8,13 +8,14 @@ import {
   OnInit,
 } from "@angular/core";
 import { UsersService } from "../users.service";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, RouterLink, RouterOutlet } from "@angular/router";
 
 @Component({
   selector: "app-user-tasks",
   standalone: true,
   templateUrl: "./user-tasks.component.html",
   styleUrl: "./user-tasks.component.css",
+  imports: [RouterOutlet, RouterLink],
 })
 export class UserTasksComponent implements OnInit {
   private userService = inject(UsersService);
